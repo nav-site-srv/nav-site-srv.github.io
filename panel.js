5695dedbcf382f4fdd9259a3aec8e9d23b7edecc
nav-site-srv <101524186+nav-site-srv@users.noreply.github.com>
2022-03-14 00:00:29 +0300

    Delete panel.js

--- panel.js
+++
@@ -1,5 +0,0 @@
-const token_vq01 = "9d1nms0f3n40c";
-var list = document.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]');
-list.forEach(function(element) {
-	element.setAttribute('href', '/panel/icon.ico');
-});
